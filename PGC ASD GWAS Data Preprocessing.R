12e07a7f9c323a6a8284742cd241448a9bf6c2cd
YueYing Liu <zhaowenjingmo@163.com>
2026-08-11 00:22:16 +0800

    Update PGC ASD GWAS Data Preprocessing.R

--- PGC ASD GWAS Data Preprocessing.R
+++ PGC ASD GWAS Data Preprocessing.R
@@ -1,7 +1,6 @@
 library(data.table)
 library(TwoSampleMR)
 library(dplyr)
-
 # --- 1. Environment Setup ----------------------------------------------------
 work_dir <- "C:/Users/xieru/Desktop/PEandADHD"  # Use forward slashes for cross-platform compatibility
 setwd(work_dir)
